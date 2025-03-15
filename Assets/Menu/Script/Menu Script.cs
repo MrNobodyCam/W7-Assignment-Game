a482b9dd5f769d7668477a2ead23c20dbdac9736
Kao Vichet <kaovichet11@gmail.com>
2025-03-15 16:29:39 +0700

    button all work right now
    button all work right now

--- Assets/Menu/Script/Menu Script.cs
+++ Assets/Menu/Script/Menu Script.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuScript : MonoBehaviour
 {
+    public void PlayNormal()
+    {
+        SceneManager.LoadSceneAsync(1);
+    }
+    public void PlayHard()
+    {
+        SceneManager.LoadSceneAsync(2);
+    }
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // void Start()
+    // {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    // }
+
+    // // Update is called once per frame
+    // void Update()
+    // {
+
+    // }
 }
